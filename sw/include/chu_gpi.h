c1d33047fad896870ff3393afe9b454eb9267f02
MarcoLerose97 <marco.lerose@hotmail.it>
2026-05-30 15:17:30 +0200

    Update chu_gpi.h

--- sw/include/chu_gpi.h
+++ sw/include/chu_gpi.h
@@ -1,34 +1,3 @@
-/******************************************************************************
-*
-* CHU GPI (GENERAL PURPOSE INPUT)
-*
-* DESCRIPTION:
-*
-*   16-bit read-only switch input peripheral.
-*
-*
-* BASE ADDRESS:
-*
-*   SW_BASE_ADDR = 0xC0000180
-*
-*
-* REGISTER MAP
-*
-*          31                             16 15             0
-*          +--------------------------------+----------------+
-* offset 0 |            reserved            | SW[15:0]       |
-*          +--------------------------------+----------------+
-*
-*
-* REGISTER SUMMARY
-*
-*   offset 0:
-*       returns current switch state
-*
-******************************************************************************/
-
-
-
 #ifndef CHU_GPI_H
 #define CHU_GPI_H
 
