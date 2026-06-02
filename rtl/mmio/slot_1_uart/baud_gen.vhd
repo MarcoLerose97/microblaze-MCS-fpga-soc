a33f59a86cf6769bffa1018e0d182d669f7e1fe2
MarcoLerose97 <marco.lerose@hotmail.it>
2026-06-02 16:11:48 +0200

    Update baud_gen.vhd

--- rtl/mmio/slot_1_uart/baud_gen.vhd
+++ rtl/mmio/slot_1_uart/baud_gen.vhd
@@ -3,43 +3,30 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity baud_gen is
-    generic(
-        DVSR     : integer := 54;
-        DVSR_BIT : integer := 6
-    );
     port(
-        clk   : in  std_logic;
-        reset : in  std_logic;
-        tick  : out std_logic
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        dvsr    : in  std_logic_vector(10 downto 0);
+        tick    : out std_logic
     );
 end baud_gen;
 
-architecture Behavioral of baud_gen is
-
-    signal r_reg  : unsigned(DVSR_BIT-1 downto 0);
-    signal r_next : unsigned(DVSR_BIT-1 downto 0);
-
+architecture arch of baud_gen is
+    constant N : integer := 11;
+    signal r_reg  : unsigned(N-1 downto 0);
+    signal r_next : unsigned(N-1 downto 0);
 begin
-
+    -- register
     process(clk, reset)
     begin
-        if reset = '1' then
-            r_reg <= (others => '0');
+        if reset='1' then
+            r_reg <= (others=>'0');
         elsif rising_edge(clk) then
             r_reg <= r_next;
         end if;
     end process;
-
-    process(r_reg)
-    begin
-        tick <= '0';
-
-        if r_reg = to_unsigned(DVSR, DVSR_BIT) then
-            r_next <= (others => '0');
-            tick   <= '1';
-        else
-            r_next <= r_reg + 1;
-        end if;
-    end process;
-
-end Behavioral;
+    -- next state logic
+    r_next <= (others=>'0') when r_reg = unsigned(dvsr) else r_reg + 1;
+    -- output logic
+    tick <= '1' when r_reg = 1 else '0';
+end arch;
