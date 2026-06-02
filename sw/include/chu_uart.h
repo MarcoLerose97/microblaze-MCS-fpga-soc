7cbda165d69d7586388ba3cbaac8822484be95d6
MarcoLerose97 <marco.lerose@hotmail.it>
2026-06-02 16:41:28 +0200

    Update chu_uart.h

--- sw/include/chu_uart.h
+++ sw/include/chu_uart.h
@@ -3,20 +3,28 @@
 
 #include <stdint.h>
 
+#define UART_DVSR_REG      1u
+#define UART_TX_REG        2u
+#define UART_RX_REMOVE_REG 3u
+
+#define UART_RX_DATA_MASK  0x000000FFu
+#define UART_RX_EMPTY_MASK 0x00000100u
+#define UART_TX_FULL_MASK  0x00000200u
+
 typedef struct {
     uint32_t base_addr;
 } uart_core_t;
 
 void uart_init(uart_core_t *dev, uint32_t base_addr);
+void uart_set_baud_rate(uart_core_t *dev, uint32_t sys_clk_hz, uint32_t baud_rate);
 
-uint32_t uart_status(uart_core_t *dev);
+uint32_t uart_read_status(uart_core_t *dev);
+uint8_t uart_rx_empty(uart_core_t *dev);
+uint8_t uart_tx_full(uart_core_t *dev);
 
-int uart_tx_fifo_full(uart_core_t *dev);
-int uart_rx_fifo_empty(uart_core_t *dev);
+void uart_write_byte(uart_core_t *dev, uint8_t data);
+uint8_t uart_read_byte(uart_core_t *dev);
 
-void uart_putc(uart_core_t *dev, char c);
-void uart_puts(uart_core_t *dev, const char *s);
-
-int uart_getc(uart_core_t *dev);
+void uart_write_string(uart_core_t *dev, const char *s);
 
 #endif
