a0fd1fbcba92f90938f4d03e216e59cfab2817c4
MarcoLerose97 <marco.lerose@hotmail.it>
2026-06-02 16:35:54 +0200

    Update uart_tx.vhd

--- rtl/mmio/slot_1_uart/uart_tx.vhd
+++ rtl/mmio/slot_1_uart/uart_tx.vhd
@@ -4,8 +4,8 @@ use ieee.numeric_std.all;
 
 entity uart_tx is
     generic(
-        DBIT    : integer := 8;
-        SB_TICK : integer := 16
+        DBIT    : integer := 8;   -- # data bits
+        SB_TICK : integer := 16   -- # ticks for stop bits
     );
     port(
         clk          : in  std_logic;
@@ -18,18 +18,15 @@ entity uart_tx is
     );
 end uart_tx;
 
-architecture Behavioral of uart_tx is
-
+architecture arch of uart_tx is
     type state_type is (idle, start, data, stop);
-
     signal state_reg, state_next : state_type;
-    signal s_reg, s_next         : unsigned(3 downto 0);
+    signal s_reg, s_next         : unsigned(4 downto 0);
     signal n_reg, n_next         : unsigned(2 downto 0);
     signal b_reg, b_next         : std_logic_vector(7 downto 0);
     signal tx_reg, tx_next       : std_logic;
-
 begin
-
+   -- FSMD state and data registers
     process(clk, reset)
     begin
         if reset = '1' then
@@ -38,7 +35,6 @@ begin
             n_reg     <= (others => '0');
             b_reg     <= (others => '0');
             tx_reg    <= '1';
-
         elsif rising_edge(clk) then
             state_reg <= state_next;
             s_reg     <= s_next;
@@ -47,8 +43,8 @@ begin
             tx_reg    <= tx_next;
         end if;
     end process;
-
-    process(state_reg, s_reg, n_reg, b_reg, tx_reg, tx_start, s_tick, din)
+    -- next state logic and data path
+    process(state_reg, s_reg, n_reg, b_reg, s_tick, tx_reg, tx_start, din)
     begin
         state_next   <= state_reg;
         s_next       <= s_reg;
@@ -89,7 +85,7 @@ begin
                         s_next <= (others => '0');
                         b_next <= '0' & b_reg(7 downto 1);
 
-                        if n_reg = DBIT - 1 then
+                        if n_reg = (DBIT - 1) then
                             state_next <= stop;
                         else
                             n_next <= n_reg + 1;
@@ -103,17 +99,14 @@ begin
                 tx_next <= '1';
 
                 if s_tick = '1' then
-                    if s_reg = SB_TICK - 1 then
+                    if s_reg = (SB_TICK - 1) then
                         state_next   <= idle;
                         tx_done_tick <= '1';
                     else
                         s_next <= s_reg + 1;
                     end if;
                 end if;
-
         end case;
     end process;
-
     tx <= tx_reg;
-
-end Behavioral;
+end arch;
