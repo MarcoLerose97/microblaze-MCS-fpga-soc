d8665a844a76a71ebc4f9f514d7255d8c3562f12
MarcoLerose97 <marco.lerose@hotmail.it>
2026-06-02 16:15:37 +0200

    Update chu_gpi.vhd

--- rtl/mmio/slot_3_gpi/chu_gpi.vhd
+++ rtl/mmio/slot_3_gpi/chu_gpi.vhd
@@ -2,15 +2,37 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity chu_gpi is
+    generic(
+        W : integer := 8    -- width of input port
+    );
     port(
-        din     : in  std_logic_vector(31 downto 0);
-        rd_data : out std_logic_vector(31 downto 0)
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        -- slot interface
+        cs      : in  std_logic;
+        write   : in  std_logic;
+        read    : in  std_logic;
+        addr    : in  std_logic_vector(4 downto 0);
+        rd_data : out std_logic_vector(31 downto 0);
+        wr_data : in  std_logic_vector(31 downto 0);
+        -- external signal
+        din     : in  std_logic_vector(W-1 downto 0)
     );
 end chu_gpi;
 
-architecture Behavioral of chu_gpi is
+architecture arch of chu_gpi is
+    signal rd_data_reg : std_logic_vector(W-1 downto 0);
 begin
-
-    rd_data <= din;
-
-end Behavioral;
+    -- input register
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            rd_data_reg <= (others => '0');
+        elsif rising_edge(clk) then
+            rd_data_reg <= din;
+        end if;
+    end process;
+    -- slot read interface
+    rd_data(W-1 downto 0)  <= rd_data_reg;
+    rd_data(31 downto W)   <= (others => '0');
+end arch;
