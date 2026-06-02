6ccfcb597d6cfcc2156b9a077b7fa0791c242089
MarcoLerose97 <marco.lerose@hotmail.it>
2026-06-02 16:15:18 +0200

    Update chu_gpo.vhd

--- rtl/mmio/slot_2_gpo/chu_gpo.vhd
+++ rtl/mmio/slot_2_gpo/chu_gpo.vhd
@@ -2,39 +2,44 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity chu_gpo is
+    generic(
+        W : integer := 16
+    );
     port(
-        clk      : in  std_logic;
-        reset    : in  std_logic;
+        clk     : in  std_logic;
+        reset   : in  std_logic;
 
-        wr_en    : in  std_logic;
-        wr_data  : in  std_logic_vector(31 downto 0);
+        cs      : in  std_logic;
+        write   : in  std_logic;
+        read    : in  std_logic;
+        addr    : in  std_logic_vector(4 downto 0);
+        rd_data : out std_logic_vector(31 downto 0);
+        wr_data : in  std_logic_vector(31 downto 0);
 
-        dout     : out std_logic_vector(31 downto 0)
+        dout    : out std_logic_vector(W-1 downto 0)
     );
 end chu_gpo;
 
-architecture Behavioral of chu_gpo is
-
-    signal reg_data : std_logic_vector(31 downto 0);
-
+architecture arch of chu_gpo is
+    signal buf_reg : std_logic_vector(W-1 downto 0);
+    signal wr_en   : std_logic;
 begin
 
     process(clk, reset)
     begin
-
         if reset = '1' then
-            reg_data <= (others => '0');
-
+            buf_reg <= (others => '0');
         elsif rising_edge(clk) then
-
             if wr_en = '1' then
-                reg_data <= wr_data;
+                buf_reg <= wr_data(W-1 downto 0);
             end if;
-
         end if;
-
     end process;
 
-    dout <= reg_data;
+    wr_en <= '1' when write = '1' and cs = '1' else '0';
 
-end Behavioral;
+    rd_data <= (others => '0');
+
+    dout <= buf_reg;
+
+end arch;
