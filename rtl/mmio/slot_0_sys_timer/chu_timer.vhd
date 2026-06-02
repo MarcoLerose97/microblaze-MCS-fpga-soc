44d6bf0114e5323c72eff9b6869229889797b5f5
MarcoLerose97 <marco.lerose@hotmail.it>
2026-06-02 15:58:54 +0200

    Update chu_timer.vhd

--- rtl/mmio/slot_0_sys_timer/chu_timer.vhd
+++ rtl/mmio/slot_0_sys_timer/chu_timer.vhd
@@ -4,81 +4,58 @@ use ieee.numeric_std.all;
 
 entity chu_timer is
     port(
-        clk      : in  std_logic;
-        reset    : in  std_logic;
-
-        wr_en    : in  std_logic;
-        wr_addr  : in  std_logic_vector(4 downto 0);
-        wr_data  : in  std_logic_vector(31 downto 0);
-
-        rd_addr  : in  std_logic_vector(4 downto 0);
-        rd_data  : out std_logic_vector(31 downto 0)
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        -- slot interface
+        cs      : in  std_logic;
+        write   : in  std_logic;
+        read    : in  std_logic;
+        addr    : in  std_logic_vector(4 downto 0);
+        rd_data : out std_logic_vector(31 downto 0);
+        wr_data : in  std_logic_vector(31 downto 0)
     );
 end chu_timer;
 
-architecture Behavioral of chu_timer is
-
-    signal counter_reg : unsigned(63 downto 0) := (others => '0');
-    signal go_reg      : std_logic := '0';
-
+architecture arch of chu_timer is
+    signal count_reg  : unsigned(47 downto 0);
+    signal count_next : unsigned(47 downto 0);
+    signal ctrl_reg : std_logic;
+    signal wr_en    : std_logic;
+    signal clear    : std_logic;
+    signal go       : std_logic;
 begin
-
+   -------- COUNTER
+   -- register
     process(clk, reset)
     begin
         if reset = '1' then
-            counter_reg <= (others => '0');
-            go_reg      <= '0';
-
+            count_reg <= (others => '0');
         elsif rising_edge(clk) then
-
-            if go_reg = '1' then
-                counter_reg <= counter_reg + 1;
-            end if;
-
-            if wr_en = '1' then
-                case wr_addr is
-
-                    -- offset 2: control register
-                    -- bit 0 = go
-                    -- bit 1 = clear
-                    when "00010" =>
-                        go_reg <= wr_data(0);
-
-                        if wr_data(1) = '1' then
-                            counter_reg <= (others => '0');
-                        end if;
-
-                    when others =>
-                        null;
-
-                end case;
-            end if;
-
+            count_reg <= count_next;
         end if;
     end process;
+    -- next state logic
+    count_next <= (others => '0') when clear = '1' else
+                  count_reg + 1   when go = '1' else
+                  count_reg;
 
-    process(rd_addr, counter_reg, go_reg)
+    -------- WRAPPING CIRCUIT
+    -- ctrl register
+    process(clk, reset)
     begin
-        rd_data <= (others => '0');
-
-        case rd_addr is
-
-            -- offset 0: counter low
-            when "00000" =>
-                rd_data <= std_logic_vector(counter_reg(31 downto 0));
-
-            -- offset 1: counter high
-            when "00001" =>
-                rd_data <= std_logic_vector(counter_reg(63 downto 32));
-
-            -- offset 2: control/status
-            when "00010" =>
-                rd_data(0) <= go_reg;
-
-            when others =>
-                rd_data <= (others => '0');
-
-        end case;
+        if reset = '1' then
+            ctrl_reg <= '0';
+        elsif rising_edge(clk) then
+            if wr_en = '1' then
+                ctrl_reg <= wr_data(0);
+            end if;
+        end if;
     end process;
-
-end Behavioral;
+    -- decoding logic
+    wr_en <= '1' when write = '1' and cs = '1' and addr(1 downto 0) = "10" else '0';
+    clear <= '1' when wr_en = '1' and wr_data(1) = '1' else '0';
+    go <= ctrl_reg;
+     -- slot read multiplexing
+    rd_data <= std_logic_vector(count_reg(31 downto 0)) when addr(0) = '0' else
+               x"0000" & std_logic_vector(count_reg(47 downto 32));
+end arch;
