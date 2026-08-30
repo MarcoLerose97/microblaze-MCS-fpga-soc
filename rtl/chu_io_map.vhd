faa4a419286851984eb2d36afc4f41633bd13a83
MarcoLerose97 <marco.lerose@hotmail.it>
2026-08-30 14:44:31 +0200

    Update chu_io_map.vhd

--- rtl/chu_io_map.vhd
+++ rtl/chu_io_map.vhd
@@ -13,7 +13,7 @@ package chu_io_map is
 
     constant S5_PWM       : integer := 5;
     constant S6_SPI       : integer := 6;
-    constant S7_SPI       : integer := 7;
+    constant S7_I2C       : integer := 7;
 
 
 end package;
