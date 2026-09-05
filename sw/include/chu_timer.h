55061e68eda830adb2a91a5e39926b4aba208b20
MarcoLerose97 <marco.lerose@hotmail.it>
2026-09-05 14:49:51 +0200

    Update chu_timer.h

--- sw/include/chu_timer.h
+++ sw/include/chu_timer.h
@@ -5,13 +5,17 @@
 
 typedef struct {
     uint32_t base_addr;
+    uint32_t ctrl;
 } timer_core_t;
 
 void timer_init(timer_core_t *dev, uint32_t base_addr);
+
 void timer_clear(timer_core_t *dev);
 void timer_start(timer_core_t *dev);
 void timer_stop(timer_core_t *dev);
+
 uint32_t timer_read_low(timer_core_t *dev);
 uint32_t timer_read_high(timer_core_t *dev);
+uint64_t timer_read_tick(timer_core_t *dev);
 
 #endif
