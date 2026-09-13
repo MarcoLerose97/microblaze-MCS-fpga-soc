e6fa341fb0c4569c778cae3659d9793ea7ab2c0e
MarcoLerose97 <marco.lerose@hotmail.it>
2026-09-13 10:35:30 +0200

    Update main.c

--- sw/src/main.c
+++ sw/src/main.c
@@ -1,30 +1,23 @@
 #include <stdint.h>
-#include "chu_uart.h"
 
-#define UART_BASE_ADDR  0xC0000200u   // Metti il vero indirizzo della UART
-#define SYS_CLK_HZ      100000000u
-#define BAUD_RATE       9600u
+#include "chu_uart.h"
+#include "chu_io_map.h"
 
 int main(void)
 {
     uart_core_t uart;
     uint8_t data;
 
-    /* Inizializzazione */
-    uart_init(&uart, UART_BASE_ADDR);
+    uart_init(&uart, UART_BASE);
 
-    /* Imposta il baud rate */
-    uart_set_baud_rate(&uart, SYS_CLK_HZ, BAUD_RATE);
+    uart_set_baud_rate(&uart, SYS_CLK_FREQ * 1000000u, 9600u);
 
-    /* Trasmette una stringa iniziale */
     uart_write_string(&uart, "UART ready\r\n");
 
     while (1)
     {
-        /* Rimane qui finché non riceve un byte */
         data = uart_read_byte(&uart);
 
-        /* Ritrasmette il byte ricevuto */
         uart_write_byte(&uart, data);
     }
 }
